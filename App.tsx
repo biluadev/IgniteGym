b0e208d42d4846c93248f0e7aa49f100765f38b8
biluadev <biluadevcod@gmail.com>
2025-04-23 13:29:30 +0100

    Auth done

--- App.tsx
+++ App.tsx
@@ -4,11 +4,10 @@ import {
   Roboto_700Bold,
   Roboto_400Regular,
 } from "@expo-google-fonts/roboto";
-import { Center, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { config } from "./config/gluestack-ui.config";
+import { Routes } from "./src/routes";
 import { Loading } from "@components/Loading";
-import { SignIn } from "@screens/SignIn";
-import { SignUp } from "@screens/SignUp";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
@@ -21,7 +20,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </GluestackUIProvider>
   );
 }
